cea1c73b0e8f3ae5ac70df87d082adb0db0fc36b
akashvaghela09 <akashvaghela09@gmail.com>
2021-05-13 10:32:11 +0530

    toast issues resolved

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
 import styles from "../Styles/Home.module.css";
 import Ripples from 'react-ripples'
+import { Fade } from "react-awesome-reveal";
 
 const Card = ({color}) => {
 
     const [toastStatus, setToastStatus] = useState(false)
    
     return (
+        
         <Ripples>
             {
                 toastStatus && 
-                        <h3 className={styles.toast}>{color} Copied</h3>
+                <Fade style={{zIndex: "100"}}>
+                <h3 className={styles.toast}>{color} Copied</h3>
+                </Fade>
             }
             <div 
                 onClick={() => {
@@ -20,7 +24,7 @@ const Card = ({color}) => {
                     }, 1200);
                     navigator.clipboard.writeText(color)
                 }} 
-
+                
                 className={styles.card} 
                 style={{background: color}} />
         </Ripples>
